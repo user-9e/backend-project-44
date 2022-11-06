d1fd5f3818f438a6b1a6df4bc2fc52a5d551e0dd
user-9e <barbara1brave@gmail.com>
2022-11-06 14:54:38 +0100

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
 export const user = readlineSync.question('May I have your name? ');
 export const greeting = `Hello, ${user}!`;
